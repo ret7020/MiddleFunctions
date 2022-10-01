d1b9b3e8079db7bef317309b9ddb537fe523ee4c
Stephan Zhdanov <55328925+ret7020@users.noreply.github.com>
2022-10-01 23:03:43 +0300

    Patch with zero and - numbers

--- f_1_5.cpp
+++ f_1_5.cpp
@@ -8,37 +8,53 @@ void itc_num_print(int number){
     std::cout << number;
 }
 
-int itc_len_num(long long number){
-    int cnt = 0;
-    while (number > 9){
-        number /= 10;
+int itc_len_num(long long number) {
+    long long cnt = 0;
+    if (number < 0)
+        number *= -1;
+    if (number == 0)
+        return 1;
+    while (number > 0) {
         cnt++;
-    }
-    return cnt + 1;
-}
-
-int itc_sum_num(long long number){
-    int sm = 0;
-    while (number > 9){
-        sm += number % 10;
         number /= 10;
     }
-    return sm + number;
+    return cnt;
 }
-long long itc_multi_num(long long number){
-    int sm = 1;
-    while (number > 9){
-        sm *= number % 10;
+
+int itc_sum_num(long long number) {
+    long long sum = 0;
+    if (number < 0)
+        number *= -1;
+    while (number > 0) {
+        sum += number % 10;
         number /= 10;
     }
-    return sm * number;
-
+    return sum;
 }
-int itc_max_num(long long number){
+
+long long itc_multi_num(long long number) {
+    long long mp = 1;
+    if (number == 0)
+        return 0;
+    if (number < 0)
+        number *= -1;
+    while (number > 0) {
+        mp *= number % 10;
+        number /= 10;
+    }
+    return mp;
+}
+
+int itc_max_num(long long number) {
     int max = -1;
-    while (number > 9){
-        if (number % 10 > max)
+    if (number == 0)
+        return 0;
+    if (number < 0)
+        number *= -1;
+    while (number > 0) {
+        if (number % 10 > max) {
             max = number % 10;
+        }
         number /= 10;
     }
     return max;
