c3556e58ded88c922a84012dc760ee28d7b60c9f
ret7020 <ret7020@gmail.com>
2022-10-03 21:28:44 +0300

    Second max -1 exception

--- f_5_10.cpp
+++ f_5_10.cpp
@@ -48,7 +48,7 @@ int itc_second_max_num(long long number) {
     if(number < 0)
         number = -number;
     if (number < 10)
-        return number;
+        return -1;
     while(number>0){
         if(second_max < number % 10 && number % 10 != max)
             second_max = number % 10;
