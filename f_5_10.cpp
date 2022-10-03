b72f43b2764c0d829cb4da6f785ac2b13b707747
ret7020 <ret7020@gmail.com>
2022-10-03 21:12:46 +0300

    Patch second max

--- f_5_10.cpp
+++ f_5_10.cpp
@@ -45,9 +45,10 @@ bool itc_mirror_num(long long number){
 int itc_second_max_num(long long number) {
     int max = itc_max_num(number);
     int second_max = 0;
-    int second;
     if(number < 0)
         number = -number;
+    if (number < 10)
+        return number;
     while(number>0){
         if(second_max < number % 10 && number % 10 != max)
             second_max = number % 10;
